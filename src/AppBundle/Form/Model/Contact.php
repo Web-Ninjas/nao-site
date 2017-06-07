1161673a4a330d21d39b94cb24646d9890c76083
ALT <alts.thomas@gmail.com>
2017-06-07 14:17:52 +0200

    Mis à jour contact

---
+++ src/AppBundle/Form/Model/Contact.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AppBundle\Form\Model;
+
+class Contact
+{
+   
+    private $nom;
+
+    private $prenom;
+
+    private $email;
+
+    private $contenu;
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * @param mixed $contenu
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+    }
+
+
+    
+}
