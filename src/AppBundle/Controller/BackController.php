6e8b392e28ca9d2f6a3c32c96c8dcd1dfed6fec1
ALT <alts.thomas@gmail.com>
2017-06-11 17:44:39 +0200

    Page Profil

--- src/AppBundle/Controller/BackController.php
+++ src/AppBundle/Controller/BackController.php
@@ -2,18 +2,52 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\ProfilType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
+
 class BackController extends Controller
 {
     /**
-     * @Route("/dashboard", name="dashboard")
+     * @Route("/dashboard/profil", name="profil")
      * @Method({"GET","POST"})
      */
-    public function dashboardAction()
+    public function profilAction(\Symfony\Component\HttpFoundation\Request $request)
     {
-        return $this->render('back/dashboard.html.twig');
+
+        $user = $this->getUser();
+
+        // Faire le formulaire et l'envoyer à la vue pour l'affichage !
+
+        $form = $this->get('form.factory')->create(ProfilType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid() )
+        {
+            /*// Si l'utilisateur a demandé à être naturaliste on modifie la propriété demandeNaturaliste en DateTime
+            if ($request->getContent('demandeNaturaliste') === '1')
+            {
+                $user->setDemandeNaturaliste(new \DateTime('now'));
+            } else {
+                $user->setDemandeNaturaliste(null);
+            }*/
+
+            // On enregistre en bdd
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('profil');
+        }
+
+        $this->addFlash('notice', 'Votre profil a bien été modifié !');
+
+        return $this->render('back/profilDashboard.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
