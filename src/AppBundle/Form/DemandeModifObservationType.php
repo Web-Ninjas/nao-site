837728cafd0abb52f1452bd394b89a25e78c8977
ALT <alts.thomas@gmail.com>
2017-07-06 23:38:13 +0200

    css demandeDeModifObservation

--- src/AppBundle/Form/DemandeModifObservationType.php
+++ src/AppBundle/Form/DemandeModifObservationType.php
@@ -4,9 +4,10 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class DemandeModifObservationType extends AbstractType
 {
@@ -16,7 +17,7 @@ class DemandeModifObservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('commentForNoValidation', TextType::class)
+            ->add('commentForNoValidation', TextareaType::class)
             ->add('envoyer', SubmitType::class);
     }
 
